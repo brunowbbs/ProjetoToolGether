27a7481fedf54e02ee68d77de115085320d8f45d
jaissonmsc <jaissonmiguelsilveira@gmail.com>
2025-05-08 21:00:23 -0300

    atualizacao

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app= express();
 app.get("/", (request, response) => {
   return response.json({ message: "Servidor funcionando!" });
 
-
+  
 
 });
+
+app.get("/login", (request, response) => {
+  return response.json({ message: "Vc fez login" });
+})
 app.listen(3333);
