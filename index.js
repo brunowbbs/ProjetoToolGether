e67cd92661d5951bf0e24fd83de5211710eff139
Wesley Bruno <engwesleybruno@gmail.com>
2025-05-08 21:02:01 -0300

    criacao da rota de teste

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 
-const app= express();
+const app = express();
 
 app.get("/", (request, response) => {
   return response.json({ message: "Servidor funcionando!" });
+});
 
-  
-
+app.get("/teste", (request, response) => {
+  return response.json({ message: "Servidor funcionando!" });
 });
 
 app.get("/login", (request, response) => {
   return response.json({ message: "Vc fez login" });
-})
+});
 app.listen(3333);
